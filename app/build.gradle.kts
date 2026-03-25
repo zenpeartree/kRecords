6839172c69796c2df4df03dd25a2dd9fa765140c
Joao Miguel Alves Pereira <joao.pereira@zendesk.com>
2026-03-25 20:17:27 +0000

    Prepare beta3 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.zenpeartree.krecords"
         minSdk = 23
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.1.0-beta2"
+        versionCode = 4
+        versionName = "0.1.0-beta3"
     }
 
     buildTypes {
